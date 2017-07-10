d2bc611d6a48de8f0e67037ddbbfe7cd928688c7
AkyChen <pubs@cbgse-iMac04-dt.local>
2017-07-10 11:11:24 -0500

    A07

---
+++ A07_dim3.r
@@ -0,0 +1,12 @@
+
+pdf(file="plot7.pdf")
+library(lattice)
+
+a <- 1:10
+b <- 1:15
+eg <- expand.grid(x=a,y=b)
+eg$z <- eg$x^2 + eg$x*eg$y
+wireframe(z~x+y, eg)
+
+t<-seq(-2*pi, 2*pi, length.out=200)
+cloud(z~x+y,data.frame(x=3*cos(t),y=3*sin(t), z=2*t))
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TestR2
+README.md
+A07_dim3.r
